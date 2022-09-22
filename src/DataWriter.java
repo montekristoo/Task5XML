06360412178ea312d58f81a0cd78be21416a74f0
kr1sto <91905647+montekristoo@users.noreply.github.com>
2022-09-23 00:20:10 +0300

    reset

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -19,6 +19,13 @@ public class DataWriter {
     public static int counter = 0;
     public static final Path manager = Path.of(PathsInfo.ROOT_PATH.toString() + PathsInfo.MANAGER_FOLDER + PathsInfo.MANAGER_FILE);
 
+    /**
+     *
+     * @param documentInfo Informations about current node, to know which link and when write to file because it's recursive approach.
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
     protected static void writeWithManager(DocumentInfo documentInfo) throws IOException, ParserConfigurationException, SAXException {
         String address = documentInfo.getAddress();
         String tag = documentInfo.getTag();
@@ -32,6 +39,7 @@ public class DataWriter {
             Element eElement = (Element) node;
             String nodeAddress = eElement.getChildNodes().item(1).getTextContent();
             System.out.println(counter);
+            System.out.println(nodeAddress);
             if (tag.equals(PathsInfo.BREAKPOINT.toString()) && aux == 0) {
                 writeToManagerAndFile(writer, nodeAddress);
                 counter++;
@@ -50,12 +58,35 @@ public class DataWriter {
         }
     }
 
+    /**
+     * @implNote reset line to read from start
+     * @throws IOException
+     */
+    private static void reset() throws IOException {
+        Path pathToFile = Path.of(PathsInfo.ROOT_PATH.toString() + PathsInfo.MANAGER_FOLDER + "//" + PathsInfo.MANAGER_FILE);
+        Files.writeString(pathToFile, "0");
+    }
+
+    /**
+     *
+     * @param fileWriter To write links to file
+     * @param url link value
+     * @throws IOException
+     */
     private static void writeToManagerAndFile(BufferedWriter fileWriter, String url) throws IOException {
        fileWriter.flush();
        fileWriter.write(url + "\n");
        Files.write(manager, String.valueOf(counter).getBytes());
     }
 
+    /**
+     *
+     * @param address - Extracted address from node
+     * @return Document
+     * @throws IOException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     */
     private static Document getParsedXMLDocument(String address) throws IOException, ParserConfigurationException, SAXException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
